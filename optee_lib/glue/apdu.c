494faabeb14160b4a02c0e3c32fc802ba1879998
Jorge Ramirez-Ortiz <jorge@foundries.io>
2021-11-14 10:39:38 +0100

    optee_lib: apdu: handle OP-TEE crypto type conversion
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- optee_lib/glue/apdu.c
+++ optee_lib/glue/apdu.c
@@ -7,12 +7,46 @@
 #include <apdu.h>
 #include <string.h>
 
+static int get_apdu_type(enum crypto_apdu_type tee_type, apduTxRx_case_t *type)
+{
+	switch (tee_type) {
+	case CRYPTO_APDU_CASE_NO_HINT:
+		*type = APDU_TXRX_CASE_INVALID;
+		break;
+	case CRYPTO_APDU_CASE_1:
+		*type = APDU_TXRX_CASE_1;
+		break;
+	case CRYPTO_APDU_CASE_2:
+		*type = APDU_TXRX_CASE_2;
+		break;
+	case CRYPTO_APDU_CASE_2E:
+		*type = APDU_TXRX_CASE_2E;
+		break;
+	case CRYPTO_APDU_CASE_3:
+		*type = APDU_TXRX_CASE_3;
+		break;
+	case CRYPTO_APDU_CASE_3E:
+		*type = APDU_TXRX_CASE_3E;
+		break;
+	case CRYPTO_APDU_CASE_4:
+		*type = APDU_TXRX_CASE_4;
+		break;
+	case CRYPTO_APDU_CASE_4E:
+		*type = APDU_TXRX_CASE_4E;
+		break;
+	default:
+		return kStatus_SSS_Fail;
+	}
+	return 0;
+}
+
 sss_status_t sss_se05x_do_apdu(Se05xSession_t *ctx,
-			       enum se050_apdu_type type,
+			       enum crypto_apdu_type tee_type,
 			       uint8_t *header, size_t hdr_len,
 			       uint8_t *src_data, size_t src_len,
 			       uint8_t *dst_data, size_t *dst_len)
 {
+	apduTxRx_case_t type = APDU_TXRX_CASE_INVALID;
 	smStatus_t status = SM_NOT_OK;
 	tlvHeader_t hdr = { 0 };
 
@@ -21,28 +55,31 @@ sss_status_t sss_se05x_do_apdu(Se05xSession_t *ctx,
 		return kStatus_SSS_Fail;
 	}
 
+	if (get_apdu_type(tee_type, &type))
+		return kStatus_SSS_Fail;
+
 	hdr.hdr[0] = *(header + 0);
 	hdr.hdr[1] = *(header + 1);
 	hdr.hdr[2] = *(header + 2);
 	hdr.hdr[3] = *(header + 3);
 
 	switch (type) {
-	case SE050_APDU_NONE:
+	case APDU_TXRX_CASE_INVALID:
 		status = DoAPDUTxRx(ctx, src_data, src_len, dst_data, dst_len);
 		break;
-	case SE050_APDU_TYPE_0:
-	case SE050_APDU_TYPE_1:
-	case SE050_APDU_TYPE_2:
+	case APDU_TXRX_CASE_1:
+	case APDU_TXRX_CASE_2:
+	case APDU_TXRX_CASE_2E:
 		status = DoAPDUTxRx_s_Case2(ctx, &hdr, src_data, src_len,
 					    dst_data, dst_len);
 		break;
-	case SE050_APDU_TYPE_3:
-	case SE050_APDU_TYPE_5:
+	case APDU_TXRX_CASE_3:
+	case APDU_TXRX_CASE_4:
 		status = DoAPDUTxRx_s_Case4(ctx, &hdr, src_data, src_len,
 					    dst_data, dst_len);
 		break;
-	case SE050_APDU_TYPE_4:
-	case SE050_APDU_TYPE_6:
+	case APDU_TXRX_CASE_3E:
+	case APDU_TXRX_CASE_4E:
 		status = DoAPDUTxRx_s_Case4_ext(ctx, &hdr, src_data, src_len,
 						dst_data, dst_len);
 		break;
--- optee_lib/glue/include/apdu.h
+++ optee_lib/glue/include/apdu.h
@@ -8,22 +8,12 @@
 #define APDU_H
 
 #include "apduComm.h"
+#include <crypto/crypto_secure_element.h>
 #include <fsl_sss_api.h>
 #include "se05x_tlv.h"
 
-enum se050_apdu_type {
-	SE050_APDU_TYPE_0 = APDU_TXRX_CASE_1,
-	SE050_APDU_TYPE_1 = APDU_TXRX_CASE_2,
-	SE050_APDU_TYPE_2 = APDU_TXRX_CASE_2E,
-	SE050_APDU_TYPE_3 = APDU_TXRX_CASE_3,
-	SE050_APDU_TYPE_4 = APDU_TXRX_CASE_3E,
-	SE050_APDU_TYPE_5 = APDU_TXRX_CASE_4,
-	SE050_APDU_TYPE_6 = APDU_TXRX_CASE_4E,
-	SE050_APDU_NONE = APDU_TXRX_CASE_INVALID,
-};
-
 sss_status_t sss_se05x_do_apdu(Se05xSession_t *ctx,
-			       enum se050_apdu_type type,
+			       enum crypto_apdu_type tee_type,
 			       uint8_t *hdr, size_t hdr_len,
 			       uint8_t *src_data, size_t src_len,
 			       uint8_t *dst_data, size_t *dst_len);
